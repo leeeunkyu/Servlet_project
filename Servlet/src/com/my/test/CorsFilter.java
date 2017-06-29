9f5d83771cddf38f0ec71d4688a29beb5241e6c5
Seyoon Park <psyoona@naver.com>
2017-06-29 20:21:09 +0900

    init setting

---
+++ Servlet/src/com/my/test/CorsFilter.java
@@ -0,0 +1,42 @@
+package com.my.test;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/*
+ * CORS 에러 방지
+ */
+public class CorsFilter implements Filter{
+
+	 @Override
+	    public void init(FilterConfig filterConfig) throws ServletException {}
+
+	    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+	    HttpServletRequest request = (HttpServletRequest) servletRequest;
+	    //System.out.println("Request"+ request.getMethod());
+
+	    HttpServletResponse resp = (HttpServletResponse) servletResponse;
+	    resp.addHeader("Access-Control-Allow-Origin","*");
+	    resp.addHeader("Access-Control-Allow-Methods","GET,POST");
+	    resp.addHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+
+	    // Just ACCEPT and REPLY OK if OPTIONS
+	    if ( request.getMethod().equals("OPTIONS") ) {
+	        resp.setStatus(HttpServletResponse.SC_OK);
+	        return;
+	    }
+	    chain.doFilter(request, servletResponse);
+	}
+
+	 @Override
+	public void destroy() {}
+	}
---
+++ Servlet/src/com/my/test/TestClass.java
@@ -0,0 +1,41 @@
+package com.my.test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestClass
+ */
+@WebServlet("/TestClass")
+public class TestClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestClass() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
